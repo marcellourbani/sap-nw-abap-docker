794cd12b30d1ada5c20057fb2522752052d47a1f
Marcello Urbani <marcello.urbani@gmail.com>
2019-09-08 22:47:59 +0100

    fedora

--- startinstaller.sh
+++ startinstaller.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run -d --privileged -v /hdd/D/local/NW752:/var/tmp/ABAP_Trial/ -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v sapdata:/sapdata --name installer -h vhcalnplci.loc.local abapinstaller:752  bash
+
+docker run -d --privileged -v /hdd/D/local/NW752:/var/tmp/ABAP_Trial/ \
+  -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v sapdata:/sapdata \
+  --name installer  -h vhcalnplci \
+  --dns-search=abap.local abapinstaller:752  bash
